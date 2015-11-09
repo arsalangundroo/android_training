a044aaafd21c3e004d6616e82e9019ca8a7a89eb
tavisca-arsalan <arsalang@tavisca.com>
2015-11-09 20:02:07 +0530

    2-pane layout

--- app/src/main/java/com/example/android/sunshine/app/DetailFragment.java
+++ app/src/main/java/com/example/android/sunshine/app/DetailFragment.java
@@ -121,6 +121,9 @@ public class DetailFragment extends Fragment implements LoaderManager.LoaderCall
     public Loader<Cursor> onCreateLoader(int id, Bundle args) {
         Log.v(LOG_TAG, "In onCreateLoader");
         Intent intent=getActivity().getIntent();
+        if (intent == null || intent.getData() == null) {
+            return null;
+        }
         return new CursorLoader(getActivity(),
                 intent.getData(),
                 DETAIL_COLUMNS,
